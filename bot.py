2b5ec5513a6b95eee8a5e918f00ed9f979a8513f
RafikovRus <Rafikov.RR@gmail.com>
2026-05-10 15:39:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -223,32 +223,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-import logging
-
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-def main() -> None:
-    application = Application.builder().token("8735371477:AAE1dRwtNTR4Uui9QdNgUV5Cu1a0TcjUfQs").build()
-
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-
-if __name__ == "__main__":
-    main()
